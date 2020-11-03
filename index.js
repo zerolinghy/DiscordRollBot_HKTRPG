14d27bcc1d6b80054caa1963ebcd10c869bb3cf1
oleelamhy <oleelamhy@gmail.com>
2020-11-03 23:23:44 +0100

    ++

--- index.js
+++ index.js
@@ -13,22 +13,3 @@ process.on('warning', (warning) => {
   console.warn(warning.message); // Print the warning message
   console.warn(warning.stack); // Print the stack trace
 });
-/*
-流程解釋
-
-首先這裡會call modules/中的Discord line Telegram 三個檔案
-如果在Heroku 有輸入它們各自的TOKEN 的話
-服務就會各自啓動
-
-Discord line Telegram三套BOT 都會統一呼叫analytics.js
-再由analytics.js 呼叫roll/ 中各個的骰檔
-
-所以基本上,要增加骰組
-參考/roll中的DEMO骰組就好
-
-以上, 有不明可以在GITHUB問我
-
-另外, 使用或參考其中代碼的話, 請保持開源
-感謝
-
-*/
