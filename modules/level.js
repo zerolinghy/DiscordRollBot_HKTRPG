f02746a739a89053c337c42a62197aa4441933cf
oleelamhy <oleelamhy@gmail.com>
2020-11-03 23:48:19 +0100

    ++

---
+++ modules/level.js
@@ -0,0 +1,130 @@
+if (!process.env.mongoURL) return;
+const oneMinuts = (process.env.DEBUG) ? 1 : 60000;
+//60000 一分鐘多久可以升級及增加經驗
+exports.rollbase = require('../roll/rollbase');
+exports.z_Level_system = require('../roll/z_Level_system');
+const schema = require('../modules/core-schema.js');
+//trpgLevelSystem
+const opt = {
+    upsert: true,
+    runValidators: true
+}
+async function EXPUP(groupid, userid, displayname, displaynameDiscord, membercount) {
+    if (!process.env.mongoURL || !Object.keys(exports.z_Level_system).length || !exports.z_Level_system.initialize().trpgLevelSystemfunction) {
+        return;
+    }
+
+    //1. 檢查GROUP ID 有沒有開啓CONFIG 功能 1
+    let userInfo = {};
+    let gpInfo = exports.z_Level_system.initialize().trpgLevelSystemfunction.find(e => e.groupid == groupid);
+    if (!gpInfo || gpInfo.Switch != 1) return;
+    if (gpInfo.trpgLevelSystemfunction) {
+        userInfo = gpInfo.trpgLevelSystemfunction.find(e => e.userid == userid)
+    }
+    if (!userInfo) {
+        await newUser(gpInfo, groupid, userid, displayname, displaynameDiscord);
+        return;
+    }
+
+    //4. 有-> 檢查上次紀錄的時間 超過60000 (1分鐘) 即增加15+(1-9) 經驗值
+    if ((new Date(Date.now()) - userInfo.LastSpeakTime) < oneMinuts) {
+        return;
+    }
+    userInfo.name = displaynameDiscord || displayname || '無名'
+    userInfo.EXP += await exports.rollbase.Dice(9) + 15;
+    userInfo.LastSpeakTime = Date.now();
+    let LVsumOne = Number(userInfo.Level) + 1;
+    let levelUP = false;
+    //5. 檢查現LEVEL 需不需要上升. =5 / 6 * LVL * (2 * LVL * LVL + 27 * LVL )+ 91DD
+    let newLevelExp = 5 / 6 * (LVsumOne) * (2 * (LVsumOne) * (LVsumOne) + 30 * (LVsumOne)) + 100;
+    if (userInfo.EXP > newLevelExp) {
+        userInfo.Level++;
+        levelUP = true;
+    }
+    //8. 更新MLAB資料
+    await uploadMongoose(groupid, userid, userInfo);
+    //6. 需要 -> 檢查有沒有開啓通知
+    if (gpInfo.Hidden != 1 || levelUP == false) return;
+    //1. 讀取LEVELUP語
+    return await returnTheLevelWord(gpInfo, userInfo, membercount);
+    //6 / 7 * LVL * (2 * LVL * LVL + 30 * LVL + 100)
+}
+
+async function returnTheLevelWord(gpInfo, userInfo, membercount) {
+    let username = userInfo.name;
+    let userlevel = userInfo.Level;
+    let userexp = userInfo.EXP;
+    let usermember_count = membercount || gpInfo.trpgLevelSystemfunction.length;
+    let userRanking = await ranking(userInfo.userid, gpInfo.trpgLevelSystemfunction);
+    let userRankingPer = Math.ceil(userRanking / usermember_count * 10000) / 100 + '%';
+    let userTitle = await exports.z_Level_system.checkTitle(userlevel, gpInfo.Title);
+    let tempUPWord = gpInfo.LevelUpWord || "恭喜 {user.name}《{user.title}》，你的克蘇魯神話知識現在是 {user.level}點了！\n現在排名是{server.member_count}人中的第{user.Ranking}名！";
+    return tempUPWord.replace(/{user.name}/ig, username).replace(/{user.level}/ig, userlevel).replace(/{user.exp}/ig, userexp).replace(/{user.Ranking}/ig, userRanking).replace(/{user.RankingPer}/ig, userRankingPer).replace(/{server.member_count}/ig, usermember_count).replace(/{user.title}/ig, userTitle);
+}
+
+async function uploadMongoose(groupid, userid, userInfo) {
+    let v = await schema.trpgLevelSystem.findOneAndUpdate({
+        groupid: groupid,
+        'trpgLevelSystemfunction.userid': userid
+    }, {
+        $set: {
+            'trpgLevelSystemfunction.$.name': userInfo.name,
+            'trpgLevelSystemfunction.$.Level': userInfo.Level
+        },
+        $max: {
+            'trpgLevelSystemfunction.$.EXP': userInfo.EXP,
+            'trpgLevelSystemfunction.$.LastSpeakTime': userInfo.LastSpeakTime
+        }
+    }, opt)
+    return v;
+}
+
+async function newUser(gpInfo, groupid, userid, displayname, displaynameDiscord) {
+    //3. 沒有 -> 新增
+    let temp = {
+        userid: userid,
+        name: displaynameDiscord || displayname || '無名',
+        EXP: await exports.rollbase.Dice(9) + 15,
+        //EXP: math.floor(math.random() * 10) + 15,
+        Level: "0",
+        LastSpeakTime: Date.now()
+    }
+    gpInfo.trpgLevelSystemfunction.push(temp)
+
+    let v = await schema.trpgLevelSystem.findOneAndUpdate({
+        groupid: groupid
+    }, {
+        $push: {
+            trpgLevelSystemfunction: temp
+        }
+    }, opt)
+    return v;
+}
+
+async function ranking(who, data) {
+    let array = [];
+    let answer = "0";
+    for (let key in data) {
+        await array.push(data[key]);
+    }
+    array.sort(function (a, b) {
+        return b.EXP - a.EXP;
+    });
+    let rank = 1;
+    for (let i = 0; i < array.length; i++) {
+        if (i > 0 && array[i].EXP < array[i - 1].EXP) {
+            rank++;
+        }
+        array[i].rank = rank;
+    }
+    for (let b = 0; b < array.length; b++) {
+        if (array[b].userid == who)
+            answer = b + 1;
+
+    }
+    return answer;
+}
+
+module.exports = {
+    EXPUP
+};
---
+++ roll/z_saveCommand.js
@@ -0,0 +1,232 @@
+"use strict";
+if (!process.env.mongoURL) {
+    return;
+}
+
+const records = require('../modules/records.js');
+var trpgCommandfunction = {};
+records.get('trpgCommand', (msgs) => {
+    trpgCommandfunction.trpgCommandfunction = msgs
+})
+const VIP = require('../modules/veryImportantPerson');
+const limitArr = [30, 200, 200, 300, 300, 300, 300, 300];
+var gameName = function () {
+    return '(公測中)儲存擲骰指令功能 .cmd (add del show 自定關鍵字)'
+}
+var gameType = function () {
+    return 'Tool:trpgCommand:hktrpg'
+}
+var prefixs = function () {
+    return [{
+        first: /(^[.]cmd$)/ig,
+        second: null
+    }]
+}
+var getHelpMessage = function () {
+    return "【儲存擲骰指令功能】" + "\n\
+這是根據關鍵字來再現擲骰指令,\n\
+例如輸入 .cmd add  pc1鬥毆 cc 80 鬥毆 \n\
+再輸入.cmd pc1鬥毆  就會執行後方的指令\n\
+add 後面第一個是關鍵字, 可以是符號或任何字\n\
+P.S.如果沒立即生效 用.cmd show 刷新一下\n\
+輸入.cmd add (關鍵字) (指令)即可增加關鍵字\n\
+輸入.cmd show 顯示所有關鍵字\n\
+輸入.cmd del(編號)或all 即可刪除\n\
+輸入.cmd  (關鍵字) 即可執行 \n\
+"
+}
+var initialize = function () {
+    return trpgCommandfunction;
+}
+
+// eslint-disable-next-line no-unused-vars
+var rollDiceCommand = async function ({
+    inputStr,
+    mainMsg,
+    groupid,
+    userrole
+}) {
+    let checkifsamename = 0
+    let rply = {
+        default: 'on',
+        type: 'text',
+        text: ''
+    };
+    let lv;
+    let limit = limitArr[0];
+    let temp = 0;
+    switch (true) {
+        case /^help$/i.test(mainMsg[1]) || !mainMsg[1]:
+            rply.text = this.getHelpMessage();
+            return rply;
+            // .cmd(0) ADD(1) TOPIC(2) CONTACT(3)
+        case /(^[.]cmd$)/i.test(mainMsg[0]) && /^add$/i.test(mainMsg[1]) && /^(?!(add|del|show)$)/ig.test(mainMsg[2]):
+            //console.log('mainMsg: ', mainMsg)
+            //增加資料庫
+            //檢查有沒有重覆
+            lv = await VIP.viplevelCheckGroup(groupid);
+            limit = limitArr[lv];
+            checkifsamename = 0
+            if (groupid && userrole >= 1 && mainMsg[3] && mainMsg[2] && mainMsg[3].toLowerCase() != ".cmd") {
+                if (trpgCommandfunction.trpgCommandfunction)
+                    for (var i = 0; i < trpgCommandfunction.trpgCommandfunction.length; i++) {
+                        if (trpgCommandfunction.trpgCommandfunction[i].groupid == groupid) {
+                            if (trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction.length >= 30) {
+                                rply.text = '關鍵字上限' + limit + '個\n支援及解鎖上限 https://www.patreon.com/HKTRPG\n或自組服務器\n源代碼  http://bit.ly/HKTRPG_GITHUB';
+                                return rply;
+                            }
+                            // console.log('checked1')
+                            if (trpgCommandfunction.trpgCommandfunction[0] && trpgCommandfunction.trpgCommandfunction[0].trpgCommandfunction[0])
+                                for (var a = 0; a < trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction.length; a++) {
+                                    if (trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction[a].topic == mainMsg[2]) {
+                                        //   console.log('checked')
+                                        checkifsamename = 1
+                                    }
+                                }
+                        }
+                    }
+                temp = {
+                    groupid: groupid,
+                    trpgCommandfunction: [{
+                        topic: mainMsg[2],
+                        contact: inputStr.replace(/\.cmd\s+add\s+/i, '').replace(mainMsg[2], '').replace(/^\s+/, '')
+                    }]
+                }
+                if (checkifsamename == 0) {
+                    records.pushtrpgCommandfunction('trpgCommand', temp, () => {
+                        records.get('trpgCommand', (msgs) => {
+                            trpgCommandfunction.trpgCommandfunction = msgs
+                            // console.log(rply);
+                        })
+
+                    })
+                    rply.text = '新增成功: ' + mainMsg[2] + '\n' + inputStr.replace(/\.cmd\s+add\s+/i, '').replace(mainMsg[2], '').replace(/^\s+/, '')
+                } else rply.text = '新增失敗. 重複標題'
+            } else {
+                rply.text = '新增失敗.'
+                if (!mainMsg[2])
+                    rply.text += ' 沒有標題.'
+                if (!mainMsg[3])
+                    rply.text += ' 沒有擲骰指令'
+                if (mainMsg[3] && mainMsg[3].toLowerCase() == ".cmd")
+                    rply.text += '指令不可以儲存.cmd啊'
+                if (!groupid)
+                    rply.text += ' 不在群組.'
+                if (groupid && userrole < 1)
+                    rply.text += ' 只有GM以上才可新增.'
+            }
+            return rply;
+
+        case /(^[.]cmd$)/i.test(mainMsg[0]) && /^del$/i.test(mainMsg[1]) && /^all$/i.test(mainMsg[2]):
+            //刪除資料庫
+            if (groupid && mainMsg[2] && trpgCommandfunction.trpgCommandfunction && userrole >= 2) {
+                for (let i = 0; i < trpgCommandfunction.trpgCommandfunction.length; i++) {
+                    if (trpgCommandfunction.trpgCommandfunction[i].groupid == groupid) {
+                        temp = trpgCommandfunction.trpgCommandfunction[i]
+                        temp.trpgCommandfunction = []
+                        records.settrpgCommandfunction('trpgCommand', temp, () => {
+                            records.get('trpgCommand', (msgs) => {
+                                trpgCommandfunction.trpgCommandfunction = msgs
+                            })
+                        })
+                        rply.text = '刪除所有關鍵字'
+                    }
+                }
+            } else {
+                rply.text = '刪除失敗.'
+                if (!groupid)
+                    rply.text += '不在群組. '
+                if (groupid && userrole < 1)
+                    rply.text += '只有GM以上才可刪除. '
+            }
+
+            return rply;
+        case /(^[.]cmd$)/i.test(mainMsg[0]) && /^del$/i.test(mainMsg[1]) && /^\d+$/i.test(mainMsg[2]):
+            //刪除資料庫
+            if (groupid && mainMsg[2] && trpgCommandfunction.trpgCommandfunction && userrole >= 1) {
+                for (let i = 0; i < trpgCommandfunction.trpgCommandfunction.length; i++) {
+                    if (trpgCommandfunction.trpgCommandfunction[i].groupid == groupid && mainMsg[2] < trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction.length && mainMsg[2] >= 0) {
+                        temp = trpgCommandfunction.trpgCommandfunction[i]
+                        temp.trpgCommandfunction.splice(mainMsg[2], 1)
+                        //console.log('trpgCommandfunction.trpgCommandfunction: ', temp)
+                        records.settrpgCommandfunction('trpgCommand', temp, () => {
+                            records.get('trpgCommand', (msgs) => {
+                                trpgCommandfunction.trpgCommandfunction = msgs
+                            })
+                        })
+                    }
+                    rply.text = '刪除成功: ' + mainMsg[2]
+                }
+            } else {
+                rply.text = '刪除失敗.'
+                if (!mainMsg[2])
+                    rply.text += '沒有關鍵字. '
+                if (!groupid)
+                    rply.text += '不在群組. '
+                if (groupid && userrole < 1)
+                    rply.text += '只有GM以上才可刪除. '
+            }
+            return rply;
+
+        case /(^[.]cmd$)/i.test(mainMsg[0]) && /^show$/i.test(mainMsg[1]):
+            //顯示
+            records.get('trpgCommand', (msgs) => {
+                trpgCommandfunction.trpgCommandfunction = msgs
+            })
+            //console.log(trpgCommandfunction.trpgCommandfunction)
+            if (!groupid) {
+                rply.text = '不在群組. ';
+                return rply
+            }
+            if (trpgCommandfunction.trpgCommandfunction)
+                for (let i = 0; i < trpgCommandfunction.trpgCommandfunction.length; i++) {
+                    if (trpgCommandfunction.trpgCommandfunction[i].groupid == groupid) {
+                        rply.text += '資料庫列表:'
+                        for (let a = 0; a < trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction.length; a++) {
+                            temp = 1
+                            rply.text += ("\n") + a + '. ' + trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction[a].topic + '\n' + trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction[a].contact + '\n'
+                        }
+                    }
+                }
+            if (temp == 0) rply.text = '沒有已設定的關鍵字. '
+            //顯示資料庫
+            rply.text = rply.text.replace(/^([^(,)\1]*?)\s*(,)\s*/mg, '$1: ').replace(/,/gm, ', ')
+            return rply
+        case /(^[.]cmd$)/i.test(mainMsg[0]) && /\S/i.test(mainMsg[1]) && /^(?!(add|del|show)$)/ig.test(mainMsg[1]):
+            //顯示關鍵字
+            if (!groupid) {
+                rply.text = '不在群組. ';
+                return rply
+            }
+            if (trpgCommandfunction.trpgCommandfunction && mainMsg[1])
+                for (let i = 0; i < trpgCommandfunction.trpgCommandfunction.length; i++) {
+                    if (trpgCommandfunction.trpgCommandfunction[i].groupid == groupid) {
+                        // console.log(trpgCommandfunction.trpgCommandfunction[i])
+                        //rply.text += '資料庫列表:'
+                        for (let a = 0; a < trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction.length; a++) {
+                            if (trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction[a].topic.toLowerCase() == mainMsg[1].toLowerCase()) {
+                                temp = 1
+                                rply.text = trpgCommandfunction.trpgCommandfunction[i].trpgCommandfunction[a].contact;
+                                rply.cmd = true;
+                            }
+                        }
+                    }
+                }
+            if (temp == 0) rply.text = '沒有相關關鍵字. '
+            rply.text = rply.text.replace(/,/mg, ' ')
+            return rply;
+        default:
+            break;
+
+    }
+}
+
+
+module.exports = {
+    rollDiceCommand: rollDiceCommand,
+    initialize: initialize,
+    getHelpMessage: getHelpMessage,
+    prefixs: prefixs,
+    gameType: gameType,
+    gameName: gameName
+};
