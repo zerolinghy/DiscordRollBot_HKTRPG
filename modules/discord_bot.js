e692832819ff5f562142d0a327546cb0aa20ed20
oleelamhy <oleelamhy@gmail.com>
2020-11-04 00:28:07 +0100

    Revert "change setPresence"
    This reverts commit 928eb979af9f33f230f58d2f8070eef3ad74c71d.

--- modules/discord_bot.js
+++ modules/discord_bot.js
@@ -355,11 +355,6 @@ client.on('shardReconnecting', id => console.log(`Shard with ID ${id} reconnecte
 
 //Set Activity 可以自定義正在玩什麼
 client.on('ready', () => {
-  client.user.setPresence({
-      game: {
-        name: 'bothelp',
-        type: 3
-      }
-  });
+	client.user.setActivity('指令請看bothelp');
 });
 client.login(channelSecret);
