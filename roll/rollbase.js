494aed1ec944a211b4680907b6675f37335bcc84
oleelamhy <oleelamhy@gmail.com>
2020-11-03 22:29:12 +0100

    update rollbase

--- roll/rollbase.js
+++ roll/rollbase.js
@@ -52,8 +52,8 @@ function BuildDiceCal(inputStr) {
 	let equation = DiceToRoll;
 	while (equation.match(/\d+d\d+/) != null) {
 		let tempMatch = equation.match(/\d+d\d+/);
-		if (tempMatch.toString().split('d')[0] > 200) return '欸欸，不支援200D以上擲骰；哪個時候會骰到兩百次以上？想被淨灘嗎？';
-		if (tempMatch.toString().split('d')[1] == 1 || tempMatch.toString().split('d')[1] > 500) return '不支援D1和超過D500的擲骰；想被淨灘嗎？';
+		if (tempMatch.toString().split('d')[0] > 200) return '欸欸，不支援200D以上擲骰';
+		if (tempMatch.toString().split('d')[1] == 1 || tempMatch.toString().split('d')[1] > 500) return '不支援D1和超過D500的擲骰';
 		equation = equation.replace(/\d+d\d+/, BuildRollDice(tempMatch));
 	}
 
@@ -95,74 +95,49 @@ function nomalDiceRoller(inputStr, text0, text1, text2) {
 
 	//先定義要輸出的Str
 	let finalStr = '';
+	let test1 = text0.match(/[(]/g) || '';
+  let test2 = text0.match(/[)]/g) || '';
+  if (test2.length != test1.length) return;
 
 
 	//是複數擲骰喔
-	if (mutiOrNot.toString().match(/\D/) == null) {
-		if (text2 != null) {
-			finalStr = text0 + '次擲骰：\n' + text1 + ' ' + text2 + '\n';
-		} else {
-			finalStr = text0 + '次擲骰：\n' + text1 + '\n';
-		}
-		if (mutiOrNot > 30) {
-			rply.text = '不支援30次以上的複數擲骰。';
-			return rply;
-		}
-		for (i = 1; i <= mutiOrNot; i++) {
-			let DiceToRoll = text1.toLowerCase();
-			if (DiceToRoll.match('d') == null) return undefined;
+	if (mutiOrNot.toString().match(/\D/i) == null && text1) {
+    if (text1.replace(/\d|[+]|[-]|[*]|[/]|[(]|[)]|[d]|[>]|[<]|[=]|[k]|[h]|[l]/ig, '')) return;
+    finalStr = text0 + '次擲骰：\n' + text1 + ' ' + (text2 || '') + '\n'
+    for (let i = 0; i < mutiOrNot; i++) {
+      finalStr += i + 1 + '# ' + await onetimeroll(text1) + '\n'
+    }
+  } else {
+    if (text0.replace(/\d|[+]|[-]|[*]|[/]|[(]|[)]|[d]|[>]|[<]|[=]|[k]|[h]|[l]/ig, '')) return;
+    finalStr = text0 + '：' + (text1 || '') + '\n'
+    finalStr += await onetimeroll(text0)
+  }
 
-			//寫出算式
-			let equation = DiceToRoll;
-			while (equation.match(/\d+d\d+/) != null) {
-				let tempMatch = equation.match(/\d+d\d+/);
-				equation = equation.replace(/\d+d\d+/, RollDice(tempMatch));
-			}
+  return finalStr;
+}
 
-			//計算算式
-			let aaa = equation;
-			aaa = aaa.replace(/\d+[[]/ig, '(');
-			aaa = aaa.replace(/]/ig, ')');
-			//aaa = aaa.replace(/[[]\d+|]/ig, "");
-			let answer = eval(aaa.toString());
+function onetimeroll(text0) {
+	let Str = ''
 
-			finalStr = finalStr + i + '# ' + equation + ' = ' + answer + '\n';
-		}
-
-	} else {
-		//一般單次擲骰
-		let DiceToRoll = mutiOrNot.toString().toLowerCase();
-		DiceToRoll = DiceToRoll.toLowerCase();
-		if (DiceToRoll.match('d') == null) return undefined;
-
-		//寫出算式
-		let equation = DiceToRoll;
-		while (equation.match(/\d+d\d+/) != null) {
-			let totally = 0;
-			let tempMatch = equation.match(/\d+d\d+/);
-			if (tempMatch.toString().split('d')[0] > 300) return undefined;
-			if (tempMatch.toString().split('d')[1] == 1 || tempMatch.toString().split('d')[1] > 1000000) return undefined;
-			equation = equation.replace(/\d+d\d+/, RollDice(tempMatch));
-
-		}
-
-		//計算算式
-		let aaa = equation;
-		aaa = aaa.replace(/\d+[[]/ig, '(');
-		aaa = aaa.replace(/]/ig, ')');
-		let answer = eval(aaa.toString());
-
-		if (text1 != null) {
-			finalStr = text0 + '：' + text1 + '\n' + equation + ' = ' + answer;
-		} else {
-			finalStr = text0 + '：\n' + equation + ' = ' + answer;
-		}
-
-	}
-
-	rply.text = finalStr;
-	return rply;
-
+  // 寫出算式
+  let equation = text0
+  while (equation.match(regex) != null) {
+    // let totally = 0
+    let tempMatch = equation.match(regex)
+    if (tempMatch[1] > 1000 || tempMatch[1] <= 0) return '不支援零顆以下及一千顆骰以上'
+    if (tempMatch[2] < 1 || tempMatch[2] > 9000000000000000) return '不支援一以下及九千兆以上'
+    equation = equation.replace(regex, await RollDice(tempMatch))
+  }
+  // 計算算式
+  let aaa = equation
+  aaa = aaa.replace(/\[.+?\]/ig, '')
+  let answer = math.eval(aaa.toString()).toString().replace(/true/i, "成功").replace(/false/i, "失敗");
+  if (equation.match(/[\s\S]{1,250}/g).length > 1) {
+    Str = answer + '（計算過程太長，僅顯示結果）';
+  } else {
+    Str = equation + ' = ' + answer
+  }
+  return Str
 
 }
 
