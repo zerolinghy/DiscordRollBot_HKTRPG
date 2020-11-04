19e21a198f08f1a16cfd7261b7035670c8368629
oleelamhy <oleelamhy@gmail.com>
2020-11-04 01:24:16 +0100

    ++

--- roll/rollbase.js
+++ roll/rollbase.js
@@ -227,7 +227,7 @@ var nomalDiceRoller = async function (text0, text1, text2) {
   if (mutiOrNot.toString().match(/\D/i) == null && text1) {
     if (text1.replace(/\d|[+]|[-]|[*]|[/]|[(]|[)]|[d]|[>]|[<]|[=]|[k]|[h]|[l]/ig, '')) return;
     finalStr = text0 + '次擲骰：\n' + text1 + ' ' + (text2 || '') + '\n'
-    if (mutiOrNot > 30) return '不支援30次以上的複數擲骰';
+    if (text0 > 30) return '不支援30次以上的複數擲骰';
     for (let i = 0; i < mutiOrNot; i++) {
       finalStr += i + 1 + '# ' + await onetimeroll(text1) + '\n'
     }
