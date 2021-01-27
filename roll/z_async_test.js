9ea92014c5eaf84582ceeeab750b1c3bb44e534c
oleelamhy <oleelamhy@gmail.com>
2021-01-27 20:32:57 +0100

    Removed .wiki .tran functions

--- roll/z_async_test.js
+++ roll/z_async_test.js
@@ -22,20 +22,26 @@ var prefixs = function () {
 
 }
 
+// var getHelpMessage = function () {
+// 	return "【Wiki查詢/即時翻譯】.wiki .image .tran .tran.(目標語言)\n\
+// 1) Wiki功能: .wiki (條目)  \n\
+// EG: .wiki BATMAN  \n\
+// 2) 圖片搜尋功能: .Image (內容)  \n\
+// 從Google 得到相關隨機圖片Link\n\
+// 隨機YES NO: 如.image yesno 會得到yes 或NO 結果\n\
+// 3) 即時翻譯功能: .Tran (內容)  \n\
+// 預設翻譯成正體中文\n\
+// EG: .tran BATMAN  \n\
+// 4) 可翻譯成其他語言: .tran.(語系) (內容)\n\
+// EG: .tran.ja BATMAN  .tran.日 BATMAN\n\
+// 常用語言代碼: 英=en, 簡=zh-cn, 德=de, 日=ja\n\
+// 語系代碼 https://github.com/vitalets/google-translate-api/blob/master/languages.js\n\
+// "
+// }
 var getHelpMessage = function () {
-	return "【Wiki查詢/即時翻譯】.wiki .image .tran .tran.(目標語言)\n\
-1) Wiki功能: .wiki (條目)  \n\
-EG: .wiki BATMAN  \n\
-2) 圖片搜尋功能: .Image (內容)  \n\
+	return "圖片搜尋功能: .image (內容)  \n\
 從Google 得到相關隨機圖片Link\n\
 隨機YES NO: 如.image yesno 會得到yes 或NO 結果\n\
-3) 即時翻譯功能: .Tran (內容)  \n\
-預設翻譯成正體中文\n\
-EG: .tran BATMAN  \n\
-4) 可翻譯成其他語言: .tran.(語系) (內容)\n\
-EG: .tran.ja BATMAN  .tran.日 BATMAN\n\
-常用語言代碼: 英=en, 簡=zh-cn, 德=de, 日=ja\n\
-語系代碼 https://github.com/vitalets/google-translate-api/blob/master/languages.js\n\
 "
 }
 var initialize = function () {
@@ -59,43 +65,43 @@ var rollDiceCommand = async function ({
 		case /^help$/i.test(mainMsg[1]) || !mainMsg[1]:
 			rply.text = this.getHelpMessage();
 			return rply;
-		case /\S+/.test(mainMsg[1]) && /[.]wiki/.test(mainMsg[0]):
-			rply.text = await wiki({
-					apiUrl: 'https://zh.wikipedia.org/w/api.php'
-				}).page(mainMsg[1].toLowerCase())
-				.then(async page => {
-					return chineseConv.tify(await page.summary())
-				}) //console.log('case: ', rply)
-				.catch(error => {
-					if (error == 'Error: No article found')
-						return '沒有此條目'
-					else {
-						return error
-					}
-				})
-			return rply;
-		case /\S+/.test(mainMsg[1]) && /^[.]tran$/.test(mainMsg[0]):
-			rply.text = await translate(inputStr.replace(mainMsg[0], ""), {
-				to: 'zh-TW'
-			}).then(res => {
-				return res.text
-			}).catch(err => {
-				return err.message;
-			});
-			return rply;
-		case /\S+/.test(mainMsg[1]) && /^[.]tran[.]\S+$/.test(mainMsg[0]):
-			lang = /.tran.(\S+)/;
-			test = mainMsg[0].match(lang)
-			rply.text = await translate(inputStr.replace(mainMsg[0], ""), {
-				to: test[1].replace("簡中", "zh-CN").replace("簡體", "zh-CN").replace(/zh-cn/ig, "zh-CN").replace("英", "en").replace("簡", "zh-CN").replace("德", "de").replace("日", "ja")
-			}).then(res => {
-				//console.log(res.from.language.iso);
-				return res.text
-			}).catch(err => {
-				console.log(err.message)
-				return err.message + "\n常用語言代碼: 英=en, 簡=zh-cn, 德=de, 日=ja\n例子: .tran.英\n.tran.日\n.tran.de";
-			});
-			return rply;
+		// case /\S+/.test(mainMsg[1]) && /[.]wiki/.test(mainMsg[0]):
+		// 	rply.text = await wiki({
+		// 			apiUrl: 'https://zh.wikipedia.org/w/api.php'
+		// 		}).page(mainMsg[1].toLowerCase())
+		// 		.then(async page => {
+		// 			return chineseConv.tify(await page.summary())
+		// 		}) //console.log('case: ', rply)
+		// 		.catch(error => {
+		// 			if (error == 'Error: No article found')
+		// 				return '沒有此條目'
+		// 			else {
+		// 				return error
+		// 			}
+		// 		})
+		// 	return rply;
+		// case /\S+/.test(mainMsg[1]) && /^[.]tran$/.test(mainMsg[0]):
+		// 	rply.text = await translate(inputStr.replace(mainMsg[0], ""), {
+		// 		to: 'zh-TW'
+		// 	}).then(res => {
+		// 		return res.text
+		// 	}).catch(err => {
+		// 		return err.message;
+		// 	});
+		// 	return rply;
+		// case /\S+/.test(mainMsg[1]) && /^[.]tran[.]\S+$/.test(mainMsg[0]):
+		// 	lang = /.tran.(\S+)/;
+		// 	test = mainMsg[0].match(lang)
+		// 	rply.text = await translate(inputStr.replace(mainMsg[0], ""), {
+		// 		to: test[1].replace("簡中", "zh-CN").replace("簡體", "zh-CN").replace(/zh-cn/ig, "zh-CN").replace("英", "en").replace("簡", "zh-CN").replace("德", "de").replace("日", "ja")
+		// 	}).then(res => {
+		// 		//console.log(res.from.language.iso);
+		// 		return res.text
+		// 	}).catch(err => {
+		// 		console.log(err.message)
+		// 		return err.message + "\n常用語言代碼: 英=en, 簡=zh-cn, 德=de, 日=ja\n例子: .tran.英\n.tran.日\n.tran.de";
+		// 	});
+		// 	return rply;
 		case /\S+/.test(mainMsg[1]) && /^[.]image$/.test(mainMsg[0]):
 			rply.text = await searchImage(inputStr, mainMsg, true)
 			rply.type = 'image'
